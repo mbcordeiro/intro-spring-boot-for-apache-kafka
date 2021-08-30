81347ddd7478693740bbf988a5deaf5bee8dbd44
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-08-30 12:10:48 -0300

    First commit

---
+++ src/main/java/com/matheuscordeiro/introspringforapachekafka/IntroSpringForApacheKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.introspringforapachekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroSpringForApacheKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroSpringForApacheKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscordeiro/introspringforapachekafka/IntroSpringForApacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.introspringforapachekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroSpringForApacheKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
